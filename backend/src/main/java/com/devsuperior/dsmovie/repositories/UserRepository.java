a87d57f6817e7a78afd83e0f3ebe72074f0aa202
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2022-01-16 22:55:38 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dsmovie.repositories;
+
+import com.devsuperior.dsmovie.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
